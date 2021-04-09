1917843cf193407ec0e620c2c59feea02c0eedac
Mikhail Budko <mikhailbudko@Mikhails-MacBook-Pro.local>
2021-04-09 00:11:06 -0400

    added endpoint for getting directions from google directions api

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,14 +1,66 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const oracledb_1 = __importDefault(require("oracledb"));
+const axios_1 = __importDefault(require("axios"));
+const cors_1 = __importDefault(require("cors"));
+oracledb_1.default.initOracleClient({ libDir: "/Users/mikhailbudko/Desktop/instantclient_19_8" });
 const app = express_1.default();
 const port = 8080;
+app.use(cors_1.default({ origin: true }));
+app.use("/directions", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.get("/", (req, res) => {
     res.send("Hello world!");
 });
+app.get("/connect", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let connection;
+    try {
+        connection = yield oracledb_1.default.getConnection({
+            user: 'mbudko',
+            password: 'Qr1Mz3Ph',
+            connectString: 'oracle.cise.ufl.edu/orcl'
+        });
+        res.send("Successfully connected to Oracle!");
+    }
+    catch (err) {
+        res.send("Error: " + err);
+    }
+    finally {
+        if (connection) {
+            try {
+                yield connection.close();
+            }
+            catch (err) {
+                res.send("Error when closing the database connection: " + err);
+            }
+        }
+    }
+}));
+app.get("/directions/:origin&:destination", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const origin = req.params.origin;
+    const destination = req.params.destination;
+    const url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&key=AIzaSyAcX2r43s91XPX5Wi2AFGFuPawqk9k74uw";
+    const response = yield axios_1.default.get(url);
+    res.send({
+        directions: response.data
+    });
+}));
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,61 @@
 import express from "express";
+import oracledb from "oracledb";
+import axios from "axios";
+import cors from "cors";
+oracledb.initOracleClient({ libDir: "/Users/mikhailbudko/Desktop/instantclient_19_8" })
 
 const app = express();
 const port = 8080; // default port to listen
+
+//setup CORS
+app.use(cors({ origin: true }));
+app.use("/directions", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
     res.send("Hello world!");
 });
+
+app.get("/connect", async (req, res) => {
+    let connection;
+    try {
+        connection = await oracledb.getConnection({
+            user: 'mbudko',
+            password: 'Qr1Mz3Ph',
+            connectString: 'oracle.cise.ufl.edu/orcl'
+        });
+        res.send("Successfully connected to Oracle!")
+    } catch (err) {
+        res.send("Error: " + err);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                res.send("Error when closing the database connection: " + err);
+            }
+        }
+    }
+
+})
+
+app.get("/directions/:origin&:destination", async (req, res) => {
+    const origin = req.params.origin;
+    const destination = req.params.destination;
+    const url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&key=AIzaSyAcX2r43s91XPX5Wi2AFGFuPawqk9k74uw"
+    const response = await axios.get(url)
+    res.send({
+        directions: response.data
+    })
+})
+
 
 // start the Express server
 app.listen(port, () => {
